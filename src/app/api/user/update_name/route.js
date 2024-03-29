9a3aaf98a65a38222a610a588b279a755716c9c1
nicohorn <nicoskate000@gmail.com>
2024-03-29 17:43:54 -0300

    Another small fix

--- src/app/api/user/update_name/route.js
+++ src/app/api/user/update_name/route.js
@@ -1,4 +1,4 @@
-import { updateName } from "@/index";
+import { updateUserName } from "@/index";
 import { NextResponse } from "next/server";
 
 export async function PATCH(req) {
@@ -6,7 +6,7 @@ export async function PATCH(req) {
     const data = await req.json(); // Parse the request body as JSON
 
     // Call the updateName function with the provided name, lastname, and email
-    const updatedUser = await updateName({ name: data.name, lastname: data.lastname, email: data.email })
+    const updatedUser = await updateUserName({ name: data.name, lastname: data.lastname, email: data.email })
 
     // Return a JSON response with the updated user data
     return NextResponse.json(updatedUser)
