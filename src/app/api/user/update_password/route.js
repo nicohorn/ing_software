fc42723ab3c56092c878fbbfd00db5b993da71f8
nicohorn <nicoskate000@gmail.com>
2024-03-30 14:54:42 -0300

    Added comments to /api/update_password route handler

--- src/app/api/user/update_password/route.js
+++ src/app/api/user/update_password/route.js
@@ -2,32 +2,32 @@ import { findUserByEmail, updateUserPassword } from "@/index";
 import { createHashBcrypt, verifyHashBcrypt } from "@/utils/hash";
 import { NextResponse } from "next/server";
 
-
 export async function PATCH(req) {
 
     const data = await req.json(); // Parse the request body as JSON
+    const oldPassword = data.oldPassword; // Extract the old password from the request data
 
-    const oldPassword = data.oldPassword
-    const user = await findUserByEmail(data.email);
-    const passwordCheck = await verifyHashBcrypt(oldPassword, user.password)
+    const user = await findUserByEmail(data.email);     // Find the user by their email
 
+    // Verify if the provided old password matches the user's stored password
+    const passwordCheck = await verifyHashBcrypt(oldPassword, user.password);
 
     if (!passwordCheck) {
-        console.log(passwordCheck)
-        return NextResponse.json({ status: 500, message: "Passwords don't match" })
+        // If the old password doesn't match, return an error response
+        return NextResponse.json({ status: 500, message: "Passwords don't match" });
     }
 
-
+    // Hash the new password using bcrypt
     const hashedPassword = await createHashBcrypt(data.newPassword);
 
-    // Call mongoose function to update the user's password 
-    const updatedUser = await updateUserPassword(data.email, hashedPassword)
+    // Call the updateUserPassword function to update the user's password in the database
+    const updatedUser = await updateUserPassword(data.email, hashedPassword);
 
-    console.log(updateUser)
     if (!updatedUser) {
-        return NextResponse.json({ status: 500, message: "Error updating user password" })
+        // If the password update fails, return an error response
+        return NextResponse.json({ status: 500, message: "Error updating user password" });
     } else {
-        return NextResponse.json({ status: 200, updatedUser: updatedUser })
+        // If the password update is successful, return a success response with the updated user object
+        return NextResponse.json({ status: 200, updatedUser: updatedUser });
     }
-
 }
