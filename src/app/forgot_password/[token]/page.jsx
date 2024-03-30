163ab8a5064ac9761a96a3779899b1b8b0ddeb63
nicohorn <nicoskate000@gmail.com>
2024-03-30 14:56:19 -0300

    Added comments to /forgot_password page

--- src/app/forgot_password/[token]/page.jsx
+++ src/app/forgot_password/[token]/page.jsx
@@ -4,13 +4,20 @@ import React from "react";
 import ForgotPasswordResetForm from "../components/ForgotPassword_ResetForm";
 
 export default function Page({ params }) {
+  // Decode the token from the URL parameters
   const token = jwtDecode(params.token);
+
+  // Extract the expiration time from the decoded token
   const expirationTime = token.exp;
+
+  // Get the current timestamp
   const currentTimestamp = Math.floor(Date.now() / 1000);
 
+  // Check if the current timestamp is less than the token's expiration time
   if (currentTimestamp < expirationTime) {
     return (
       <div className="flex min-h-screen flex-col items-center justify-between">
+        {/* Render the ForgotPasswordResetForm component */}
         <ForgotPasswordResetForm
           email={token.email}
           currentTimestamp={currentTimestamp}
@@ -23,7 +30,10 @@ export default function Page({ params }) {
     return (
       <div className="flex min-h-screen flex-col items-center justify-between">
         <div className="flex flex-col gap-2 items-center">
+          {/* Display a message indicating that the token has expired */}
           <h1 className="p-2 bg-primary rounded-md shadow-md">Token expired</h1>
+
+          {/* Render a link to the forgot password page */}
           <Link
             className="text-white text-xs hover:underline"
             href="/forgot_password"
